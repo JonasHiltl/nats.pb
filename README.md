e0a8641331c12bad2bf32dab2a8a8515c3805cde
Jonas Hiltl <jonashiltl2003@gmail.com>
2023-04-25 22:57:52 +0200

    Init package folder structure

---
+++ README.md
@@ -0,0 +1,7 @@
+# gRPC-NATS
+
+### Generating go code
+```
+go build protoc-gen-nats/main.go
+buf generate
+```
---
+++ protoc-gen-nats/transport.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"time"
+
+	"github.com/nats-io/nats.go"
+)
+
+type Transport interface {
+	Request(subj string, data []byte, timeout time.Duration) (*nats.Msg, error)
+}
+
+type NatsTransport struct {
+	nc *nats.Conn
+}
+
+func (t *NatsTransport) Request(subj string, data []byte, timeout time.Duration) (*nats.Msg, error) {
+	return t.nc.Request(subj, data, timeout)
+}
+
+func NewNatsTransport(nc *nats.Conn) Transport {
+	return &NatsTransport{
+		nc: nc,
+	}
+}
